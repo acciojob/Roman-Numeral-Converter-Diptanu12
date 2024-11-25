f548e52499bdcbe11213465552efda9dce5da021
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-11-25 22:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 function convertToRoman(num) {
-    const obj = {
-        0: ['M', 1000],
-        1: ['D', 500],
-        2: ['C', 100],
-        3: ['L', 50],
-        4: ['X', 10],
-        5: ['V', 5],
-        6: ['I', 1]
-    };
+    const numerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
     let romanNumerals = '';
-    const numerals = Object.values(obj);
 
-    for (let i = 0; i < numerals.length; i++) {
-        const [symbol, value] = numerals[i];
-        const count = Math.floor(num / value);
-        if (count > 0) {
-            romanNumerals += symbol.repeat(count);
-            num %= value; 
-        }
-
-        if (i < numerals.length - 1) {
-            const [nextSymbol, nextValue] = numerals[i + 1];
-            if (num >= value - nextValue && nextValue < value) {
-                romanNumerals += nextSymbol + symbol;
-                num -= value - nextValue;
-            }
+    for (const [symbol, value] of numerals) {
+        while (num >= value) {
+            romanNumerals += symbol;
+            num -= value;
         }
     }
 
     return romanNumerals;
 }
 
-console.log(convertToRoman(14));  
-console.log(convertToRoman(798)); 
-console.log(convertToRoman(36)); 
+// Examples
+console.log(convertToRoman(14));    
+console.log(convertToRoman(58));   
+console.log(convertToRoman(1994));
 
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+// Do not edit below this line
+module.exports = convertToRoman;
